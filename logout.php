1f6218c285a251c6feea2091d317737f612428ad
Juan Sebastian Piamba Borrero <juanpiamba23@gmail.com>
2024-12-01 13:05:17 -0500

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+session_start(); // Iniciar la sesión
+session_unset(); // Limpiar todas las variables de sesión
+session_destroy(); // Destruir la sesión
+header("Location: login.php"); // Redirigir a la página de inicio de sesión
+exit();
+?>
