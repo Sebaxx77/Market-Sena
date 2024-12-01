c2a377b6f3ca6b1e247c1fc57ed37922406ca3ef
Juan Sebastian Piamba Borrero <juanpiamba23@gmail.com>
2024-12-01 12:49:59 -0500

    Update config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+// Configuración de la base de datos
+$servername = "localhost";
+$username = "Tu Usuario";
+$password = "";
+$dbname = "MarketSenadb";
+
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
